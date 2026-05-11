61755ffef40d6bf69f0351f395112c1db6478009
Lukasz Bola <lukasz.bola@icloud.com>
2026-05-11 18:59:17 +0200

    fix: add error handler for server startup failure

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,11 @@ import './env'
 import { app } from './app'
 import { env } from './env'
 
-app.listen(env.PORT, () => {
+const server = app.listen(env.PORT, () => {
   console.log(`Server running on port ${env.PORT}`)
+})
+
+server.on('error', err => {
+  console.error('Server failed to start:', err)
+  process.exit(1)
 })
